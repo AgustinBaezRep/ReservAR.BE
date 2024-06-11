eb1c4d1f32941909d141994e536a4f94e43fbeca
Agustin Baez <agustinramirobaez@hotmail.com>
2024-06-11 16:32:46 -0300

    Added Specification Pattern bases and one example for Bookings

--- ReservAR.Infraestructure/Persistance/Repositories/BookingRepository.cs
+++ ReservAR.Infraestructure/Persistance/Repositories/BookingRepository.cs
@@ -1,4 +1,8 @@
-﻿using ReservAR.Application.IRepositories;
+﻿using Microsoft.EntityFrameworkCore;
+using ReservAR.Application.Helpers.Specifications;
+using ReservAR.Application.Helpers.Specifications.BookingSpecifications;
+using ReservAR.Application.IRepositories;
+using ReservAR.Domain.Booking;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +13,29 @@ namespace ReservAR.Infraestructure.Persistance.Repositories
 {
     public class BookingRepository : IBookingRepository
     {
+        private readonly ReservarDbContext _context;
 
+        public BookingRepository(ReservarDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Booking?>> GetTopThreePriceLowerBookings(TopThreePriceLowerSpecification specification, CancellationToken cancellationToken)
+        {
+            return await ApplyBookingSpecification(specification)
+                .Take(3)
+                .ToListAsync(cancellationToken);
+        }
+
+        public async Task<Booking?> GetLowerCostBooking(PriceLowerBookingSpecification specification, CancellationToken cancellationToken)
+        {
+            return await ApplyBookingSpecification(specification)
+                .FirstOrDefaultAsync(cancellationToken);
+        }
+
+        private IQueryable<Booking?> ApplyBookingSpecification(Specification<Booking> specification)
+        {
+            return SpecificationQueryBuilder.GetQuery(_context.Bookings, specification);
+        }
     }
 }
